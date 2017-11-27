a9784a1d7484c00bc3df4e77b00b0959469ee8b1
Merlinda Sumardi <merlinda.sumardi@gmail.com>
2017-11-27 23:13:06 +0700

    skill

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response} from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-skills',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
+  private apiUrl = 'http://localhost:3000/skill';
+  skill: any = {};
 
-  constructor() { }
+  constructor(private http: Http) {
+    console.log('Skill Page');
+    this.getData();
+    this.getSkill();
+  }
 
   ngOnInit() {
   }
-
+  getData() {
+    return this.http.get(this.apiUrl)
+      .map((res: Response) => res.json());
+  }
+  getSkill() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.skill = {skills: data};
+    });
+  }
 }
